7a27fc15a65f2e1e3fbdffb3261ccbacdcff8e08
Aeros <aerossalaga@gmail.com>
2021-03-10 11:54:39 +0800

    update theme header and footer

--- wp-content/themes/platforma_kit/footer.php
+++ wp-content/themes/platforma_kit/footer.php
@@ -11,21 +11,6 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'platforma_kit' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'platforma_kit' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'platforma_kit' ), 'platforma_kit', '<a href="https://aerossalaga.com/">Aeros Salaga</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
